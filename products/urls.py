fc725b8e5b3685bc0f441e1520d4da301da00f82
Dickson <olejemedickson@gmail.com>
2024-04-29 00:54:06 +0100

    Add delete product confirmation

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('all', views.PostList.as_view(), name='all-products'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('<slug:product_slug>/', views.product_detail_by_slug, name='product_detail_by_slug'),
+    path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
     
 ]

fabbc6505bddbf05df42ffb7a704e65e163dd9fc
Dickson <olejemedickson@gmail.com>
2024-04-29 15:56:35 +0100

    Add search functionality

--- zitmall/urls.py
+++ zitmall/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('products/', include('products.urls')),
+    path('summernote/', include('django_summernote.urls')),
     path('', include('home.urls')),
 ]

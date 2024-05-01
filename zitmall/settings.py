4adab5b4602f4fca24eee0605c130f3fa989b383
Dickson <olejemedickson@gmail.com>
2024-05-01 17:27:38 +0100

    Add working checkout on stripe

--- zitmall/settings.py
+++ zitmall/settings.py
@@ -204,10 +204,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 FREE_DELIVERY_THRESHOLD = 500
 STANDARD_DELIVERY_PERCENTAGE =10
-# STRIPE_CURRENCY = 'usd'
-# STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
-# STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
-# STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
+STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
     
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'

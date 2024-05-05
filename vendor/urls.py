26e0567362d8ddce3d607cf51d29bde0ab83be10
Dickson <olejemedickson@gmail.com>
2024-05-05 05:15:05 +0100

    Add order history to vendor

--- vendor/urls.py
+++ vendor/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.vendor_admin, name='vendor_admin'),
     path('add/', views.add_product, name='add_product'),
+    path('orders/', views.vendor_orders, name='vendor_orders'),
     path('products/', views.all_products, name='vendor_products'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
--- vendor/views.py
+++ vendor/views.py
@@ -8,6 +8,7 @@ from django.utils.text import slugify
 
 from products.models import Product, Category
 from products.forms import ProductForm
+from checkout.models import Order, OrderLineItem
 
 # Create your views here.
 
@@ -64,28 +65,6 @@ def all_products(request):
 
     return render(request, 'vendor/all-products.html', context)
 
-
-def product_detail(request, product_id):
-    """ A view to show individual product details """
-
-    product = get_object_or_404(Product, pk=product_id)
-
-    context = {
-        'product': product,
-    }
-
-    return render(request, 'vendor/product_detail.html', context)
-
-def product_detail_by_slug(request, product_slug):
-    """ A view to show individual product details """
-
-    product = get_object_or_404(Product, slug=product_slug)
-
-    context = {
-        'product': product,
-    }
-
-    return render(request, 'vendor/product_detail.html', context)
 
 @login_required
 def add_product(request):
@@ -156,3 +135,55 @@ def delete_product(request, product_id):
     product.delete()
     messages.success(request, 'Product deleted!')
     return redirect(reverse('vendor_products'))
+
+
+@login_required
+def vendor_orders(request):
+    """ A view to show all vendor orders, including sorting and search queries """
+
+    orders = OrderLineItem.objects.filter(product__vendor=request.user)
+    query = None
+    categories = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('title'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+            
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(title__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
+
+    context = {
+        'orders': orders,
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
+    }
+
+    return render(request, 'vendor/orders.html', context)
+

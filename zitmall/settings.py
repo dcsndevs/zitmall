80b9f76a0ad8e89206c4b409287911e9be748936
Dickson <olejemedickson@gmail.com>
2024-05-09 16:51:08 +0100

    Enable shopping cart on nav

--- zitmall/settings.py
+++ zitmall/settings.py
@@ -131,16 +131,16 @@ WSGI_APPLICATION = 'zitmall.wsgi.application'
 #         }
 #     }
 
-# DATABASES = {
-#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-# }
-
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
+
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
 
 
 # Password validation

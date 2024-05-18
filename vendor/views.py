6799a1352816d1464119a02d2811169d90af89d5
Dickson <olejemedickson@gmail.com>
2024-05-18 14:43:15 +0100

    Change name to title for products

--- vendor/views.py
+++ vendor/views.py
@@ -1,7 +1,6 @@
 from functools import wraps
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
-from django.contrib.auth.decorators import staff_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 from django.contrib.auth.models import User

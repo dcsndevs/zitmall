9c3093bfc0b1bd25a4d1d13735a6b80eb639a166
Dickson <olejemedickson@gmail.com>
2024-05-18 19:39:46 +0100

    Add auto create user for annonymous checkout

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,5 +1,7 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from django.contrib.auth.models import User
+
 
 from .models import OrderLineItem
 
@@ -16,3 +18,14 @@ def update_on_delete(sender, instance, **kwargs):
     Update order total on lineitem delete
     """
     instance.order.update_total()
+    
+
+@receiver(post_save, sender=OrderLineItem)
+def create_user_after_purchase(sender, instance, created, **kwargs):
+    """
+    Automatically create a superuser account for the user after they have successfully purchased.
+    """
+    if created:  # Check if a new purchase was created
+        user_email = instance.order.email
+        if not User.objects.filter(email=user_email).exists():
+            User.objects.create_user(username=user_email, email=user_email, password=instance.order.order_number)
